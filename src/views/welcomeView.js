dad3ed6a7870273680ca2fe24a66bb4321a40667
Tugbanur <tnur.yurtsever@gmail.com>
2024-08-19 22:21:02 +0200

    modified

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -7,7 +7,7 @@ import { START_QUIZ_BUTTON_ID, INPUT_NAME_ID } from '../constants.js';
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
-    <h1 class='welcomeTitle'>Welcome</h1>
+    <h1 class='welcome-title'>Welcome</h1>
     <h2>2024 Olympics Quiz</h2>
     <input id="${INPUT_NAME_ID}" type="text"  name="name" placeholder="Enter your name" required>
     <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
