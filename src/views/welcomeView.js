b1af1a51fc3bfde8c68885b8003c64a347f839ec
Tugbanur <tnur.yurtsever@gmail.com>
2024-08-22 09:59:28 +0200

    added keyup to enter

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -13,12 +13,16 @@ export const createWelcomeElement = () => {
     <input id="${INPUT_NAME_ID}" type="text"  name="name" placeholder="Enter your name" required>
     <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
   `;
-
-  element
-    .querySelector(`#${START_QUIZ_BUTTON_ID}`)
-    .addEventListener('click', () => {
-      const playerName = element.querySelector(`#${INPUT_NAME_ID}`).value;
-      quizData.playerName = playerName;
+  
+  const startButton = element.querySelector(`#${START_QUIZ_BUTTON_ID}`);
+    startButton.addEventListener('click', () => {
+  const playerName = element.querySelector(`#${INPUT_NAME_ID}`).value;
+    quizData.playerName = playerName;
+  });
+    element.querySelector(`#${INPUT_NAME_ID}`).addEventListener('keyup', (event) => {
+      if (event.code === 'Enter') {
+        startButton.click();
+      }
     });
   return element;
 };
